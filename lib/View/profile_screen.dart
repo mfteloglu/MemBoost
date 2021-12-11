67dff3d1508f1fa1fba0a0fea14c6f6508b1daef
umitbasak <umit.basak123@gmail.com>
2021-12-11 20:05:03 +0300

    Added basic profile screen template

--- lib/View/profile_screen.dart
+++ lib/View/profile_screen.dart
@@ -12,8 +12,162 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Profile'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Profile",
+          style: TextStyle(color: Colors.black87),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            margin: const EdgeInsets.only(top: 30.0),
+            child: const HeatMap(),
+          ),
+          Container(
+            //padding: const EdgeInsets.all(10.0),
+            //margin: const EdgeInsets.only(top: 30.0),
+            child: const Text(
+              "Today",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            //margin: const EdgeInsets.only(top: 30.0),
+            child: const Text(
+              "Studied x cards\nRetention rate is %x",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          Container(
+            //padding: const EdgeInsets.all(10.0),
+            margin: const EdgeInsets.only(top: 10.0),
+            child: const Text(
+              "1 Month",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            //margin: const EdgeInsets.only(top: 30.0),
+            child: const Text(
+              "Studied x cards\nRetention rate is %x",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          Container(
+            //padding: const EdgeInsets.all(10.0),
+            margin: const EdgeInsets.only(top: 10.0),
+            child: const Text(
+              "All time",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            //margin: const EdgeInsets.only(top: 30.0),
+            child: const Text(
+              "Studied x cards\nRetention rate is %x",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+      extendBodyBehindAppBar: true,
+    );
+  }
+}
+
+class HeatMap extends StatelessWidget {
+  const HeatMap({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      shrinkWrap: true,
+      padding: const EdgeInsets.all(10),
+      crossAxisCount: 15,
+      crossAxisSpacing: 5,
+      mainAxisSpacing: 5,
+      children: [
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+        HeatMapNode(),
+      ],
+    );
+  }
+}
+
+class HeatMapNode extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      //child: const Text("HeatMapNode"),
+      color: Colors.green,
     );
   }
 }
